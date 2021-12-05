08db2f0fe13bb57bb4b2cfd0f1a852ef9b72f395
Jennifer Cole <capauldi@gmail.com>
2021-12-05 05:18:32 -0800

    saves to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 var currDayEl = $("#currentDay");
 var timeBlockEl = $("#timeblocks");
 
+// Storage
+var savedSchedule = [];
+if ("schedule" in localStorage){
+    savedSchedule = JSON.parse(localStorage.getItem("schedule"));
+}
+
 // Format date as "Weekday, Month 0, 0000"
 var displayDate = function(date) {
     dateDisplay = date.format("dddd, MMMM D, YYYY");
@@ -29,7 +35,7 @@ $("#prev").on("click", function() {
         var date = moment().subtract(1, "days"); // Make day yesterday
         dateNav.currentDay = dateNumber(date);
         $(this).prop("disabled", true); // Hide previous button when you go back a day
-    } else if (dateNav.currentDay == (today + 1)) { // If tomorrow
+    } else if (dateNav.currentDay == dateNumber(moment().add(1, "days"))) { // If tomorrow
         date = moment(); // Make day today
         dateNav.currentDay = today;
     } else if (dateNav.currentDay > today) { // If any day after tomorrow
@@ -43,6 +49,9 @@ $("#prev").on("click", function() {
     // Display date on schedule
     displayDate(date);
     currDayEl.text(dateDisplay);
+    // Display schedule for current day
+    displaySchedule();
+    // Return date change
     return dateNav;
 });
 // Next day button
@@ -62,81 +71,137 @@ $("#next").on("click", function() {
     // Display date on schedule
     displayDate(date);
     currDayEl.text(dateDisplay);
+    // Display schedule for current day
+    displaySchedule();
+    // Return date change
     return dateNav;
 });
+
+// Save to local storage
+var saveTask = function() {
+    localStorage.setItem("schedule", JSON.stringify(savedSchedule));
+}
 
 // Set a clear date for auto-clearing localstorage
 var clearDate = dateNumber(moment().subtract(2, "days"));
 console.log(clearDate);
 
-// Dynamically creating time blocks so I'm not manually putting in 9 divs with the same classes and stuff
-var startHour = 9; // Start calendar at 9 AM
-var displayHoursAmount = 9; // Go until 5 PM
+// Create time blocks and its buttons/content
+var displaySchedule = function() {
+    $(".time-block").remove(); // Start with clean page
 
-for (var i = 0; i < displayHoursAmount; i++) {
-    var thisHour = i + startHour;
-    // Create the date row
-    var dateRow = $("<div>")
-        .addClass("time-block row");
+    var startHour = 9; // Start calendar at 9 AM
+    var displayHoursAmount = 9; // Go until 5 PM
 
-    // Create the block for the date
-    var dateBlock = $("<div>")
-        .addClass("hour col-2 col-sm-1 p-3 m-0 text-right")
-    // Uses moment to display the hour in 12 hour clock format with AM/PM
-        .text(moment().hour(thisHour).format("h A"));
+    for (var i = 0; i < displayHoursAmount; i++) {
+        var thisHour = i + startHour; // Hour on a 24 hour clock
+        // Create the date row
+        var dateRow = $("<div>")
+            .addClass("time-block row")
+            .attr("hour", thisHour);
 
-    // Create the block for the actual schedule content
-    var scheduleBlock = $("<div>")
-        .addClass("sched-block d-flex col-8 col-sm-10 text-left p-0 m-0"); // 10 columns with bootstrap
+        // Create the block for the date
+        var dateBlock = $("<div>")
+            .addClass("hour col-2 col-sm-1 p-3 m-0 text-right")
+        // Uses moment to display the hour in 12 hour clock format with AM/PM
+            .text(moment().hour(thisHour).format("h A"));
 
-    // Find out the current hour
-    var currentHour = moment().get("hour");
+        // Create the block for the actual schedule content
+        var scheduleBlock = $("<div>")
+            .addClass("sched-block d-flex col-8 col-sm-10 text-left p-0 m-0"); // 10 columns with bootstrap
 
-    // Change background color depending on the relative time by adding classes
-    if (thisHour == currentHour) {
-        scheduleBlock.addClass("present"); // Present; if row hour = current hour
-    } else if (thisHour < currentHour) {
-        scheduleBlock.addClass("past"); // Past; if row hour is less than current hour number
-    } else if (thisHour > currentHour) {
-        scheduleBlock.addClass("future"); // Future; if row hour is more than current hour number
+        // Find out the current hour
+        var currentHour = moment().get("hour");
+
+        // Change background color depending on the relative time by adding classes
+        if (thisHour == currentHour) {
+            scheduleBlock.addClass("present"); // Present; if row hour = current hour
+        } else if (thisHour < currentHour) {
+            scheduleBlock.addClass("past"); // Past; if row hour is less than current hour number
+        } else if (thisHour > currentHour) {
+            scheduleBlock.addClass("future"); // Future; if row hour is more than current hour number
+        }
+
+        // Create p element for schedule text content
+        var scheduleBlockP = $("<p>").addClass("p-3 m-0");
+        scheduleBlock.append(scheduleBlockP);
+
+        // If anything is saved in localstorage, apply text...
+        if (savedSchedule.length > 0) {
+            for (var n = 0; n < savedSchedule.length; n++) {
+                // If current day AND if current time block matches what's in localstorage, add text
+                if (savedSchedule[n].day == dateNav.currentDay && savedSchedule[n].data.time == thisHour){
+                    scheduleBlockP.text(savedSchedule[n].data.text);
+                    break;
+                }
+            }
+        }
+
+        // Create a save button
+        var saveBtn = $("<button>");
+        var saveIcon = $("<i>"); // This page uses fontawesome, which uses i to display their icons
+        saveIcon.addClass("fas fa-save"); // Fontawesome i classes for save button
+        saveBtn.addClass("saveBtn col-2 col-sm-1 p-3 m-0").append(saveIcon); // Add icon to button
+
+        timeBlockEl.append(dateRow); // Add each row to page
+        dateRow.append(dateBlock) // Add time block to row
+            .append(scheduleBlock) // Add schedule block to row
+            .append(saveBtn); // Add save button to row
     }
 
-    var scheduleBlockP = $("<p>").addClass("p-3 m-0").data("hour", thisHour);
-    scheduleBlock.append(scheduleBlockP);
+    // Create edit on click
+    $(".sched-block").on("click", function() {
+        // Find text of clicked upon schedule block
+        var text = $(this).text().trim();
 
-    // Create a save button
-    var saveBtn = $("<button>");
-    var saveIcon = $("<i>"); // This page uses fontawesome, which uses i to display their icons
-    saveIcon.addClass("fas fa-save"); // Fontawesome i classes for save button
-    saveBtn.addClass("saveBtn col-2 col-sm-1 p-3 m-0").append(saveIcon); // Add icon to button
+        // Make new textarea and add the text into it
+        var textInput = $("<textarea>").addClass("col-12 m-0 p-3").val(text);
+        textInput.height(this.scrollHeight);
 
-    timeBlockEl.append(dateRow); // Add each row to page
-    dateRow.append(dateBlock) // Add time block to row
-        .append(scheduleBlock) // Add schedule block to row
-        .append(saveBtn); // Add save button to row
+        // Find and replace p with textarea and focus on it
+        $(this).find("p").replaceWith(textInput);
+        textInput.trigger("focus");
+    });
+    $(".sched-block").on("blur", "textarea", function() {
+        // Text is whatever is typed into textarea
+        var text = $(this).val().trim();
+
+        // Replace textarea with p and add text
+        var scheduleBlockP = $("<p>").addClass("p-3 m-0").text(text);
+        $(this).replaceWith(scheduleBlockP);
+    });
+
+    // What happens when you click the save button
+    $(".time-block").on("click", "button", function() {
+        // create an object to save info in
+        var dataPerDay = {
+            day: dateNav.currentDay, // Current day number
+            data: {
+                time: $(this).parent().attr("hour"), // Current hour
+                text: $(this).parent().find("p").text() // Text content
+            }
+        };
+        // If nothing has been saved yet
+        if (savedSchedule.length == 0 ) {
+            savedSchedule.push(dataPerDay);
+        }
+        // If there is stuff saved, loop through it
+        for (var n = 0; n < savedSchedule.length; n++) {
+            if (savedSchedule[n].day == dataPerDay.day && savedSchedule[n].data.time == dataPerDay.data.time) {
+                // If day and time matches something in array, update text
+                savedSchedule[n].data.text = dataPerDay.data.text;
+                break;
+            } else {
+                // If new, push into array
+                savedSchedule.push(dataPerDay);
+                break;
+            }
+        }
+        // Save it to localstorage
+        saveTask();
+    });
 }
-
-// Create edit on click
-$(".sched-block").on("click", function() {
-    // Find text of clicked upon schedule block
-    var text = $(this).text().trim();
-
-    // Make new textarea and add the text into it
-    var textInput = $("<textarea>").addClass("col-12 m-0 p-3").val(text);
-    textInput.height(this.scrollHeight);
-
-    // Find and replace p with textarea and focus on it
-    $(this).find("p").replaceWith(textInput);
-    textInput.trigger("focus");
-});
-$(".sched-block").on("blur", "textarea", function() {
-    // Text is whatever is typed into textarea
-    var text = $(this).val().trim();
-
-    // Replace textarea with p and add text
-    var scheduleBlockP = $("<p>").addClass("p-3 m-0").text(text);
-    $(this).replaceWith(scheduleBlockP);
-});
+displaySchedule(); // Run on page load
 
 /*// Open calendar
 currDayEl.on("click", function() {
